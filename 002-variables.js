7e0c809ca3b01d57103592bff4bb039b3c88713d
Bipesh <bipeshkhadka2003@gmail.com>
2023-01-22 17:17:12 +0545

    added variable course

---
+++ 002-variables.js
@@ -0,0 +1,13 @@
+console.log("program to understand variables in javascript")
+
+// we can use var let or const to declear variables
+let a = 8
+console.log(a)
+a = "Bipesh khadka"
+console.log(a)  // we can change the variable types in js 
+// js is case sesnsitive language and here is eg:
+
+var harry = 7
+var haRRy = 5
+console.log(harry)
+console.log(haRRy)
