26da68c9e9aa5a09503aa9cccbc95ee5d161e798
Bipesh <bipeshkhadka2003@gmail.com>
2023-01-23 21:51:28 +0545

    practise set of js 1st

---
+++ 01_practiseSet.js
@@ -0,0 +1,40 @@
+console.log("practise set of javascript")
+
+//1st qno add number in var type of string
+
+let a = "harry boy"
+let b = 5
+let c
+c = a + b
+console.log(a)
+console.log(b)
+console.log(c)
+
+//use typeof operator to find the datatype of last string
+console.log(typeof c)
+
+// 1 qno- 3
+const student =
+  {
+    name:"harry bahi",
+    age:17,
+    id:BigInt(759273490),
+    isteacher:false
+  }
+//student{}   so we cannot define new object in object
+student["hobby"]="footballer", student["garde"]=1 , student["gf"]="all girls"
+console.log(student)
+
+// qno -4
+
+// word : meaning
+
+const dictionary =
+  {
+    Abnegation : "renunciation of a doctrine or belief",
+Ambigue: "An ambiguous expression or statement",
+Athleisure: "Comfortable and casual footwear & clothing designed", 
+Broigus: "Angry or irritated",
+Broigus: "Angry or irritated"
+  }
+console.log(dictionary)
