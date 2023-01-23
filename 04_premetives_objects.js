c5dd65a4e49dff7d645e63b53db389f94021e0d6
Bipesh <bipeshkhadka2003@gmail.com>
2023-01-23 21:22:54 +0545

    object and premitive datatype in js

---
+++ 04_premetives_objects.js
@@ -0,0 +1,23 @@
+console.log("Premitive datatypes nn ss bb u")
+let a = null
+let b = 2743
+let c = Symbol('i am a good boy')
+let d = "hello world"
+let e = false
+let f = BigInt(2837468) + BigInt(83475)
+let g = undefined
+
+console.log(a, b, c, d, e, f, g)
+console.log(typeof c)
+
+
+// object in javascript
+console.log("non premetive datatype eg: object")
+
+const data = {
+  "name": "harry",
+  "age": 14,
+  "gender": "male"
+}
+console.log(data["name"])
+console.log(data)
