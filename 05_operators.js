6884a322ded91f1be70a5f3f3a4eeaf0d22786a2
Bipesh <bipeshkhadka2003@gmail.com>
2023-01-24 19:17:38 +0545

    added operator course

---
+++ 05_operators.js
@@ -0,0 +1,34 @@
+// operators in javascript
+
+//arithmetic operators
+let a = 5
+let b = 10
+console.log("a + b =", a+b)
+console.log("a - b =", a-b)
+console.log("a * b =", a*b)
+console.log("a / b =", a/b)
+console.log("a % b =", a%b)
+console.log("a ** b =", a**b)
+console.log("a ++ =", a++)
+console.log("++a =", ++a)
+console.log("--b =", --b)
+console.log("b-- =", b--)
+console.log(b)
+
+// conditional operators
+ a = 6
+ b = 10
+console.log("a == b",a==b)
+console.log("a != b",a!=b)
+console.log("a !== b",a!==b)
+console.log("a === b",a===b)
+console.log("a <= b",a<=b)
+console.log("a > b",a>b)
+
+// logical opeartors
+
+console.log(a>b||a<b)
+console.log(a>b&&a<b)
+console.log(a>b!=a<b)
+
+
