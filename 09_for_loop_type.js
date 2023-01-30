60f9134ab0c9bd468ffb64fbadf6bb71f79146c0
Bipesh <bipeshkhadka2003@gmail.com>
2023-01-30 07:52:03 +0545

    commit of for in loop and for of loop

---
+++ 09_for_loop_type.js
@@ -0,0 +1,16 @@
+// for_ in_ loop
+let obj ={
+     bipesh:98,
+     harri:74,
+     ritika:88,
+     geeta:83,
+    }
+    for(let a in obj)
+      {
+        console.log("marks of " +a+ " are " +obj[a])
+      }
+    
+    for(let a of " hello world")  // for of loop is used for 
+      {
+        console.log(a)
+      }
