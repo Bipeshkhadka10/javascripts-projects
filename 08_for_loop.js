424541df58f87622dafba7ab85aff77efd81bbac
Bipesh <bipeshkhadka2003@gmail.com>
2023-01-30 06:54:56 +0545

    commit of for loop

---
+++ 08_for_loop.js
@@ -0,0 +1,10 @@
+// program of for loop:
+let sum = 0;
+let n = prompt("enter the number");
+for(let i=1; i<n; i++)
+{
+     sum += i;
+
+}
+console.log("the sum of " +n+ "natural number is " +sum);
+//console.log(i)    here variable is declared by let which is bock scope
